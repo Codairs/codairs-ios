3bc244748f3aca9545c9ab14e54e613f2aa51168
Hamon Ben Riazy <hamon.riazy@gmail.com>
2016-06-18 20:59:18 +0100

    installed firebase

---
+++ Pods/Target Support Files/Pods-Codairs/Pods-Codairs-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CodairsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CodairsVersionString[];
+
---
+++ Pods/Target Support Files/Pods-CodairsTests/Pods-CodairsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodairsTests : NSObject
+@end
+@implementation PodsDummy_Pods_CodairsTests
+@end
