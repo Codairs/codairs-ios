7a0cb7d98f62d44a29d006c2f2eb42f2c8d227a1
Hamon Ben Riazy <hamon.riazy@gmail.com>
2016-06-19 10:34:42 +0100

    new icon and appreciations

---
+++ Source/ViewController/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  Codairs
+//
+//  Created by Hamon Riazy on 19/06/2016.
+//  Copyright © 2016 Hamon Riazy. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
